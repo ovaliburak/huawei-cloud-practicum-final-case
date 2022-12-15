551dbf31cc522e89bff1a0a5a3c80d0e45e93037
Burak Ovali <burakovalii@outlook.com>
2022-12-15 07:08:14 +0300

    customer retrieve func added

--- src/customer/app/core/views.py
+++ src/customer/app/core/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from rest_framework.generics import ListAPIView
+from rest_framework.generics import RetrieveAPIView
 from core.serializers import CustomerEmployeeListSerializer
 from core.models import Customer
+
 class CustomerListAPIView(ListAPIView):
     serializer_class=CustomerEmployeeListSerializer
     queryset = Customer.objects.all()
+
+class CustomerRetrieveAPIView(RetrieveAPIView):
+    serializer_class=CustomerEmployeeListSerializer
+    queryset=Customer.objects.all()
